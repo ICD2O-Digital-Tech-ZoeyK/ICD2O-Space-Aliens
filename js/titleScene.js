dc08bac5de4383df46069f60848e607b9fd8dba3
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-05-27 14:09:40 +0000

    Space aliens #6

--- js/titleScene.js
+++ js/titleScene.js
@@ -35,7 +35,7 @@ class TitleScene extends Phaser.Scene {
      */
     preload() {
         console.log("Title Scene")
-        this.preload.image("titleSceneBackground", "assets/aliens_screen_image.jpg")
+        this.load.image("titleSceneBackground", "assets/aliens_screen_image.jpg")
     }
 
     /** 
@@ -52,7 +52,7 @@ class TitleScene extends Phaser.Scene {
 
         this.titleSceneText = this.add
             .text(1920 / 2, 1080 / 2 + 350, "Space Aliens", this.titleSceneTextStyle)
-        .setOrigin(0.5)
+            .setOrigin(0.5)
     }
 
     /**

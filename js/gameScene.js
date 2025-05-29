ad985f6959ee8babd68c2ac2728dc0be93ef7ed6
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-05-29 14:07:13 +0000

    made a web page called and about Genesis

---
+++ js/gameScene.js
@@ -0,0 +1,37 @@
+/* global Phaser */
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This is the Game Scene
+class GameScene extends Phaser. Scene {
+    constructor () {
+        super({ key: 'gameScene' })
+
+        this.background = null
+        this.ship = null
+    }
+
+    init(data) {
+        this.cameras.main.setBackgroundColor("#ffffff")
+    }
+
+    preload () {
+        console.log('Game Scene')
+        
+        // images
+        this.load.image('starBackground', 'assets/starBackground.png')
+        this.load.image('ship', 'assets/spaceShip.png')
+    }
+
+    create(data) {
+        this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+        this.background.setOrigin(0, 0)
+        
+        this.Ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+    }
+        update(time, delta) {
+        }
+}
+
+export default GameScene
--- js/menuScene.js
+++ js/menuScene.js
@@ -8,37 +8,37 @@
 
 class MenuScene extends Phaser. Scene {
     constructor () {
-        super({ key: 'menuScene' })
+        super({ key: "menuScene" })
 
         this.menuSceneBackgroundImage = null
         this.startButton = null
     }
 
     init(data) {
-        this.cameras.main.setBackgroundColor('#ffffff')
+        this.cameras.main.setBackgroundColor("#ffffff")
     }
 
     preload () {
-        console.log('Menu Scene')
-        this.load.image('menuSceneBackground', 'assets/aliens_screen_image2.jpg')
-        this.load, image('startButton', 'assets/start.png')
+        console.log("Menu Scene")
+        this.load.image("menuSceneBackground", "assets/aliens_screen_image2.jpg")
+        this.load, image("startButton", "assets/start.png")
     }
 
     create(data) {
-        this.menuSceneBackgroundImage = this.add sprite(0, 0, 'menuSceneBackground')
+        this.menuSceneBackgroundImage = this.add.sprite(0, 0, "menuSceneBackground")
         this.menuSceneBackgroundImage.x = 1920 / 2
         this.menuSceneBackgroundImage.y = 1080 / 2
 
-        this.StartButton = this.add sprite(1920 / 2, (1080 / 2) + 100, 'startButton')
-        this.startButton.setInteractive({ useHandCursor: true f)
-        this - startButton.on('pointerdown', () → this.clickButton())
+        this.StartButton = this.add.sprite(1920 / 2, (1080 / 2) + 100, "startButton")
+        this.startButton.setInteractive({ useHandCursor: true })
+        this.startButton.on("pointerdown", () => this.clickButton())
     }
 
     update(time, delta) {
     }
     
     clickButton() {
-        this.scene.start('gameScene')
+        this.scene.start("gameScene")
     }
 }
 export default MenuScene

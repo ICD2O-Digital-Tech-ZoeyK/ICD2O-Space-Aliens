f4a726314d84c8204796f77fabb63fb09c4e2019
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-05-29 14:34:26 +0000

    made a web page called and about Genesis

--- js/gameScene.js
+++ js/gameScene.js
@@ -30,8 +30,28 @@ class GameScene extends Phaser. Scene {
         
         this.Ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
     }
-        update(time, delta) {
+    update(time, delta) {
+        //called 60 times a second, hopefully!
+        
+        const keyLeftObj = this.input.keyboard.addKey("LEFT")
+        const keyRightObj = this.input.keyboard.addkey("RIGHT")
+
+        if (keyLeftObj.isDown === true) {
+                this.ship.x -= 15
+            if (this.ship.x < 0) {
+                    this.ship.x = 0
+            }
+
         }
+        
+        if (keyRightObj.isDown === true) {
+            this.ship.x += 15
+            if (this.ship.x > 1920) {
+                this.ship.x = 1920
+            }
+
+        }
+    }
 }
 
 export default GameScene

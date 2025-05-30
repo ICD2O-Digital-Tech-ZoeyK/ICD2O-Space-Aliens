c011774f2872e9ee915aabd41c4c4c34e3f0667c
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-05-30 14:02:22 +0000

    made a web page called and about Genesis

--- js/gameScene.js
+++ js/gameScene.js
@@ -47,7 +47,7 @@ class GameScene extends Phaser. Scene {
         if (keyLeftObj.isDown === true) {
             this.ship.x -= 15
             if (this.ship.x < 0) {
-                    this.ship.x = 0
+                this.ship.x = 0
             }
 
         }

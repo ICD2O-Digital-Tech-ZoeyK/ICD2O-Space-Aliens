09cb24b51818d9013397abda95ce9692be0e0c94
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-05-30 14:48:59 +0000

    made a web page called and about Genesis

--- js/gameScene.js
+++ js/gameScene.js
@@ -31,7 +31,7 @@ class GameScene extends Phaser. Scene {
         this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
         this.background.setOrigin(0, 0)
         
-        this.Ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+        this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
         //create a group for the missiles
         this.missileGroup = this.physics.add.group()

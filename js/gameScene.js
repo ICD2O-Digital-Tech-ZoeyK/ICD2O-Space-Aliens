45d56e42d135d6889f723573d1a58300d8717a5a
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-06-04 14:51:27 +0000

    made a web page called and about Genesis

--- js/gameScene.js
+++ js/gameScene.js
@@ -40,8 +40,9 @@ class GameScene extends Phaser.Scene {
         // images
         this.load.image("starBackground", "assets/starBackground.png")
         this.load.image("ship", "assets/spaceShip.png")
-        this.load.image("missile", "assets/missile.gif")
         this.load.image("alien", "assets/alien.png")
+        this.load.gif("missile", "assets/missile.gif")
+        this.load.gif("alien", "assets/alien.gif")
         // sounds
         this.load.audio("laser", "assets/laser1.wav")
         this.load.audio("explosion", "assets/barrelExploding.wav")
